b3cc5fdcd0559ffdd0f1b85c0d3759709f5dfe2f
Alamaos <alamaosoficial@gmail.com>
2023-07-05 15:17:43 +0000

    obj_handler commented

--- animation/obj_handler.py
+++ animation/obj_handler.py
@@ -1,38 +1,36 @@
 def parse_obj_file(filename):
     """
-
-    :param filename:
-    :return:
+    Função para analisar um ficheiro '.obj' e extrair os vértices e faces
+    :param filename: Nome do ficheiro '.obj' a ser analisado
+    :return: Uma tuplo contendo as listas de vértices e faces do ficheiro
     """
-    vertices = []  # Inicializar a lista para armazenar os vértices
-    faces = []  # Inicializar a lista para armazenar as faces
+    vertices = []  # Lista para armazenar os vértices
+    faces = []  # Lista para armazenar as faces
 
-    # Abrir o arquivo .obj para leitura
+    # Abertura do ficheiro '.obj' para leitura
     for line in open(filename, 'r'):
-        # Ignorar comentários
+        # Ignorar comentários dentro do ficheiro
         if line.startswith('#'):
             continue
-        # Dividir a linha em valores separados por espaço
+        # Dividir as linhas em valores separados por um espaço
         values = line.split()
-        # Se a linha estiver vazia, ignorá-la
+        # Se a linha estiver vazia, irá ser ignorada
         if not values:
             continue
 
-        # Se a linha começar com 'v', então é uma definição de vértice
+        # Se a linha começar com 'v', significa que é um vértice
         if values[0] == 'v':
             # Adicionar os valores de vértice à lista (convertidos para float)
             vertices.append(list(map(float, values[1:4])))
-        # Se a linha começar com 'f', então é uma definição de face
+        # Se a linha começar com 'f', significa que é uma face
         elif values[0] == 'f':
-            face = []  # Inicializar a lista para armazenar os índices dos vértices da face
+            face = []  # Inicialização da lista para armazenar os índices dos vértices da face
             # Para cada índice de vértice na definição de face
             for v in values[1:]:
-                # Os índices podem ser separados por '/', então precisamos dividir novamente
+                # Divisão de índices por '/'
                 w = v.split('/')
-                # Subtrair 1 dos índices porque o Python indexa a partir de 0,
-                # enquanto os arquivos .obj indexam a partir de 1
+                # Subtrair 1, porque o Python indexa a partir de 0 enquanto os arquivos .obj indexam a partir de 1
                 face.append(int(w[0]) - 1)
             # Adicionar a face à lista de faces
             faces.append(face)
-    # Retornar as listas de vértices e faces
     return vertices, faces
