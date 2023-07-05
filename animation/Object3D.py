6c373ccbc51aabde63d8d043d64b3a7d3c9846cc
Alamaos <alamaosoficial@gmail.com>
2023-07-05 15:46:16 +0000

    Objeto3D comment

--- animation/Object3D.py
+++ animation/Object3D.py
@@ -1,3 +1,4 @@
+# Secção de Importações
 from obj_handler import parse_obj_file
 import numpy as np
 from operations import translate, rotate_around_object_y, rotate_around_object_x, rotate_around_object_z
@@ -8,18 +9,18 @@ from transformation import apply_perspective_projection
 class Object3D:
     def __init__(self, obj_file_path, position, fov, aspect_ratio, near, far, canvas_width, canvas_height, start_angle):
         """
-
-        :param obj_file_path:
-        :param position:
-        :param fov:
-        :param aspect_ratio:
-        :param near:
-        :param far:
-        :param canvas_width:
-        :param canvas_height:
-        :param start_angle:
+        Inicializa um objeto 3D com as seguintes propriedades:
+        :param obj_file_path: Caminho para o arquivo '.obj' contendo os vértices e faces do objeto
+        :param position: Posição inicial do objeto no espaço 3D (x, y, z)
+        :param fov: Campo de visão da projeção de perspetiva
+        :param aspect_ratio: Razão de aspeto da janela de renderização
+        :param near: Distância do plano de corte próximo
+        :param far: Distância do plano de corte distante
+        :param canvas_width: Largura do canvas de renderização
+        :param canvas_height: Altura do canvas de renderização
+        :param start_angle: Ângulo inicial de rotação do objeto
         """
-        # Carregar vértices e faces do arquivo .obj
+        # Carregar vértices e faces do ficheiro '.obj'
         self.original_vertices, self.faces = parse_obj_file(obj_file_path)
         # Definir a posição inicial do objeto 3D
         self.x, self.y, self.z = position
@@ -34,26 +35,26 @@ class Object3D:
         self.canvas_width = canvas_width
         self.canvas_height = canvas_height
         # Definir a cor do objeto como azul
-        self.r, self.g, self.b = 33, 70, 94  # Blue color
-        self.moon_radius = 10
+        self.r, self.g, self.b = 33, 70, 94  # Cor planeta Terra
+        self.moon_radius = 10  # Radio de translação da Lua com base na Terra
 
     def get_animated_polygons(self, step, axis='y'):
         """
-
-        :param step:
-        :param axis:
-        :return:
+        Retorna uma lista de polígonos a serem renderizados na animação atual
+        :param step: Valor do incremento do ângulo de rotação
+        :param axis: Eixo de rotação ("x", "y" ou "z")
+        :return: Lista de polígonos para renderização
         """
-        # Update the rotation angle
+        # Atualiza o ângulo de rotação
         self.angle += step
 
-        # Copy the original vertices for transformation
+        # Copia os vértices originais para a transformação
         self.vertices = np.copy(self.original_vertices)
 
-        # Move the vertices to the object's position
+        # Move os vértices para a posição do objeto
         self.vertices = translate(self.vertices, [self.x, self.y, self.z])
 
-        # Apply rotations around the axes
+        # Aplica rotações em torno dos eixos
         if axis == 'x':
             self.vertices = rotate_around_object_x(self.vertices, self.angle * (pi / 180))
         elif axis == 'y':
@@ -61,47 +62,47 @@ class Object3D:
         elif axis == 'z':
             self.vertices = rotate_around_object_z(self.vertices, self.angle * (pi / 180))
 
-        # Apply perspective projection to the vertices
+        # Aplica projeção de perspetiva nos vértices
         self.vertices = apply_perspective_projection(self.vertices, self.fov, self.aspect_ratio, self.near, self.far)
 
-        # Map vertices to screen coordinates
+        # Mapeia os vértices para coordenadas da tela
         screen_vertices = [(x * self.canvas_width / 2 + self.canvas_width / 2, -y * self.canvas_height / 2 +
                             self.canvas_height / 2, z) for x, y, z in self.vertices]
 
-        # Initialize the list to store polygons
+        # Inicializa a lista para armazenar os polígonos
         polygons = []
         fill_color = '#%02x%02x%02x' % (self.r, self.g, self.b)
-        # For each face, generate the corresponding polygons
+        # Para cada face, gera os polígonos correspondentes
         for face in self.faces:
-            if len(face) > 3:  # If the face has more than 3 vertices, generate multiple triangles
+            if len(face) > 3:  # Se a face tiver mais de 3 vértices, gera vários triângulos
                 for i in range(1, len(face) - 1):
                     vertices = [screen_vertices[face[0]], screen_vertices[face[i]], screen_vertices[face[i + 1]]]
                     polygons.append((vertices, fill_color))
-            else:  # Otherwise, generate a single polygon for the face
+            else:  # Caso contrário, gera um único polígono para a face
                 vertices = [screen_vertices[i] for i in face]
                 polygons.append((vertices, fill_color))
 
-        # Calculate moon's position and angle based on the orbit axis
-        moon_angle = -self.angle * (pi / 180)  # Invert the sign of the moon's angle
+        # Calcula a posição e o ângulo da lua com base no eixo da órbita
+        moon_angle = -self.angle * (pi / 180)  # Inverte o sinal do ângulo da lua
         moon_x = self.x + self.moon_radius * cos(moon_angle)
         moon_y = self.y
         moon_z = self.z + self.moon_radius * sin(moon_angle)
         moon_position = [moon_x, moon_y, moon_z]
 
-        # Translate, rotate, and project the moon vertices
+        # Translada, rotaciona e projeta os vértices da lua
         moon_vertices = translate(self.original_vertices, moon_position)
         moon_vertices = rotate_around_object_y(moon_vertices, -self.angle * (pi / 180))
         moon_vertices = translate(moon_vertices, [self.x, self.y, self.z])
         moon_vertices = apply_perspective_projection(moon_vertices, self.fov, self.aspect_ratio, self.near, self.far)
 
-        # Map moon vertices to screen coordinates
+        # Mapeia os vértices da lua para coordenadas da tela
         screen_moon_vertices = [(x * self.canvas_width / 2 + self.canvas_width / 2, -y * self.canvas_height / 2 +
                                  self.canvas_height / 2, z) for x, y, z in moon_vertices]
 
-        # Define the fill color for the moon
-        moon_fill_color = '#cccccc'  # Color for the moon
+        # Define a cor de preenchimento para a lua
+        moon_fill_color = '#cccccc'
 
-        # Generate polygons for the moon
+        # Gera polígonos para a lua
         for face in self.faces:
             if len(face) > 3:
                 for i in range(1, len(face) - 1):
@@ -113,5 +114,5 @@ class Object3D:
                 vertices = [screen_moon_vertices[i] for i in face]
                 polygons.append((vertices, moon_fill_color))
 
-        # Return the polygons for both the Earth and the Moon
+        # Retorna os polígonos tanto para a Terra quanto para a Lua
         return polygons
