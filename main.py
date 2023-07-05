c63934d1267c1c681b06c48507f4fc69f497e39f
Alamaos <alamaosoficial@gmail.com>
2023-07-05 15:04:16 +0000

    Organizing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+# Secção de Importações
+import subprocess
 
+
+# Função que irá correr a animação
+def main():
+    subprocess.run(["python", "./animation/animation.py"])
 
 
 if __name__ == "__main__":
-   main()
+    main()
