b963c2038a30f7c6251c662f5496716b229b785c
Alamaos <alamaosoficial@gmail.com>
2023-07-05 15:37:39 +0000

    transformation comment

--- animation/transformation.py
+++ animation/transformation.py
@@ -4,12 +4,10 @@ from math import cos, sin, tan
 
 def translation(v):
     """
-
-    :param v:
-    :return:
+    Realiza uma translação de um vetor no espaço 3D
+    :param v: Vetor de translação [dx, dy, dz]
+    :return: Matriz de translação 4x4
     """
-    # Esta função cria uma matriz de translação com base num vetor de translação v.
-    # 'v' é uma lista ou array de valores representando a translação em cada dimensão.
     size = len(v)
     value = np.eye(size + 1)
     value[:-1, -1] = v
@@ -18,12 +16,10 @@ def translation(v):
 
 def dilation(v):
     """
-
-    :param v:
-    :return:
+    Realiza uma dilatação de um vetor no espaço 3D
+    :param v: Vetor de dilatação [sx, sy, sz]
+    :return: Matriz de dilatação 4x4
     """
-    # Esta função cria uma matriz de dilatação com base num vetor de dilatação v.
-    # 'v' é uma lista ou array de valores representando a dilatação em cada dimensão.
     size = len(v)
     value = np.eye(size + 1)
     value[:-1, :-1] = np.diag(v)
@@ -32,12 +28,10 @@ def dilation(v):
 
 def rotation_x(angle):
     """
-
-    :param angle:
-    :return:
+    Realiza uma rotação em torno do eixo x no espaço 3D
+    :param angle: Ângulo de rotação em graus
+    :return: Matriz de rotação 4x4
     """
-    # Esta função cria uma matriz de rotação em torno do eixo X com base num ângulo fornecido.
-    # 'angle' é o ângulo de rotação em graus.
     angle = np.radians(angle)
     value = np.array([[1, 0, 0, 0],
                       [0, np.cos(angle), -np.sin(angle), 0],
@@ -48,12 +42,10 @@ def rotation_x(angle):
 
 def rotation_y(angle):
     """
-
-    :param angle:
-    :return:
+    Realiza uma rotação em torno do eixo y no espaço 3D
+    :param angle: Ângulo de rotação em graus
+    :return: Matriz de rotação 4x4
     """
-    # Esta função cria uma matriz de rotação em torno do eixo Y com base num ângulo fornecido.
-    # 'angle' é o ângulo de rotação em graus.
     return np.array([[cos(angle), 0, sin(angle), 0],
                      [0, 1, 0, 0],
                      [-sin(angle), 0, cos(angle), 0],
@@ -62,12 +54,10 @@ def rotation_y(angle):
 
 def rotation_z(angle):
     """
-
-    :param angle:
-    :return:
+    Realiza uma rotação em torno do eixo z no espaço 3D
+    :param angle: Ângulo de rotação em graus
+    :return: Matriz de rotação 4x4
     """
-    # Esta função cria uma matriz de rotação em torno do eixo Z com base num ângulo fornecido.
-    # 'angle' é o ângulo de rotação em graus.
     angle = np.radians(angle)
     value = np.array([[np.cos(angle), -np.sin(angle), 0, 0],
                       [np.sin(angle), np.cos(angle), 0, 0],
@@ -78,21 +68,14 @@ def rotation_z(angle):
 
 def apply_perspective_projection(rotated_vertices, fov, aspect_ratio, near, far):
     """
-
-    :param rotated_vertices:
-    :param fov:
-    :param aspect_ratio:
-    :param near:
-    :param far:
-    :return:
+    Aplica a projeção de perspetiva aos vértices rotacionados
+    :param rotated_vertices: Lista de vértices rotacionados [(x, y, z), ...]
+    :param fov: Campo de visão em radianos
+    :param aspect_ratio: Razão de aspeto da tela (largura/altura)
+    :param near: Plano de corte próximo
+    :param far: Plano de corte distante
+    :return: Lista de vértices projetados [(x_proj, y_proj, z_proj), ...]
     """
-    # Esta função aplica a projeção perspetiva a vértices rotacionados.
-    # 'rotated_vertices' são os vértices após as transformações de rotação.
-    # 'fov' é o campo de visão em radianos.
-    # 'aspect_ratio' é a razão entre a largura e a altura da tela.
-    # 'near' e 'far' são os planos de corte da câmara,
-    # representando as distâncias mínima e máxima, respetivamente, que a câmara pode ver.
-
     # Cria a matriz de projeção perspetiva.
     proj_matrix = [
         [aspect_ratio * (1 / tan(fov / 2)), 0, 0, 0],
@@ -103,14 +86,14 @@ def apply_perspective_projection(rotated_vertices, fov, aspect_ratio, near, far)
 
     projected_vertices = []
 
+    # Converte o vértice para coordenadas homogéneas
     for vertex in rotated_vertices:
         x, y, z = vertex
-        # Converte o vértice para coordenadas homogêneas.
         vertex_4d = [x, y, z, 1]
-        # Aplica a matriz de projeção ao vértice.
+        # Aplica a projeção de perspetiva aos vértices
         vertex_proj = [sum([vertex_4d[i] * proj_matrix[j][i] for i in range(4)]) for j in range(4)]
 
-        # Converte de volta para coordenadas 3D se o valor de w (vertex_proj[3]) for diferente de 0.
+        # Divide pelos componentes homogéneos para obter as coordenadas projetadas
         if vertex_proj[3] != 0:
             vertex_proj = [i / vertex_proj[3] for i in vertex_proj[:-1]]
         else:
